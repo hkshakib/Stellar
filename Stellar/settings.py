83801665f0298f36d714d80ebd7180c6e752bb97
hkshakib <humayunkibriashakib@gmail.com>
2023-05-28 15:59:48 +0600

    feat: Added a new app named contact, fix: fixed a bug on listings serializers

--- Stellar/settings.py
+++ Stellar/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'accounts',
     'realestate',
     'listings',
+    'contacts',
 ]
 
 MIDDLEWARE = [
--- listings/serializers.py
+++ listings/serializers.py
@@ -15,7 +15,7 @@ class ListingSerializer(serializers.ModelSerializer):
         model = Listing
         fields = (
             'title', 'address', 'city', 'state', 'price', 'sale_type', 'home_type', 'bedrooms', 'bathrooms', 'sqft',
-            'photo_main', 'slug')
+            'slug', 'photos')
 
 
 class ListingDetailSerializer(serializers.ModelSerializer):
