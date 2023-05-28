8008147f07c174146d366a2746f1f0de295dd22d
hkshakib <humayunkibriashakib@gmail.com>
2023-05-28 21:16:55 +0600

    feat: install redux and router, added necessary containers name, Home, Contact, About, Listing, ListingsDetails, Login and SignUp, Also Added NotFound Components for invalid route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Layout from "./Hocs/Layout";
-const App = () => (
+function App() {
+  return (
     <Layout>
-
     </Layout>
-);
+  );
+}
 
 export default App;
